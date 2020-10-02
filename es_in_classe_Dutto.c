3d63f28e0ee465aae9689e5b1b4f47c1b68ef2c4
Alessandro Dutto <71831228+Aledutto23@users.noreply.github.com>
2020-10-02 09:22:36 +0200

    Add files via upload

---
+++ es_in_classe_Dutto.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int  R, N, contagi, x, giornitot;
+    int giorni = 0;
+    int array[] = {};
+
+    printf("inserisci R: ");
+    scanf("%d", &R);
+
+
+
+    printf("inserisci N: ");
+    scanf("%d", &N);
+
+    for (giorni=0; giorni<N; giorni++){
+
+        giornitot=giorni*R;
+
+
+    }
+
+    printf("i giorni totali sono %d", &giorni);
+
+
+
+
+
+
+}
