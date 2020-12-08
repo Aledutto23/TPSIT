a93ec2dcde07a09be9328a1d0f4f631ad0bcf62e
Alessandro Dutto <71831228+Aledutto23@users.noreply.github.com>
2020-12-08 15:51:35 +0100

    Update es_spotify.c

--- es_spotify.c
+++ es_spotify.c
@@ -8,24 +8,44 @@
 #define SONG 1000
 #define TITOLOFILE "playlist.csv"
 
-typedef struct Slista{                                            //struttura necessaria per ogni canzone
+typedef struct lista{                                            //struttura necessaria per ogni canzone
     int numero;
     char *titolo, *artista;
     bool ordine;
-}lista;
+    int val;
+    struct lista *next ;
+}Lista;
 
-void casuale(lista* playlist, int cont);
+void push(Lista *head, int valore){
+    Lista *punt_appoggio;
+    punt_appoggio = head;
+    while(punt_appoggio->next != NULL){
+        punt_appoggio = punt_appoggio->next;
+    }
+    punt_appoggio->next = (Lista*) malloc(sizeof(Lista));
+    punt_appoggio->next->val = valore;
+    punt_appoggio->next->next = NULL;
+
+}
+
+void casuale(Lista* playlist, int cont);
 
 int main(){
 
-lista playlist[SONG];
+
+Lista playlist[SONG];
 char riga[MAX];
 int cont = 0;                                              //inizializzare cont a 0;
 FILE *fp = fopen("playlist.csv", "r");
 
+Lista * elemento_1;
+     elemento_1 = (Lista*)malloc(sizeof(Lista));
+     elemento_1->val = 1;
+     elemento_1->next = NULL;
+
 if (fp == NULL)                                                 //ciclo if per verificare se il file viene aperto correttmente
 {
-    printf("il file non è stato aperto correttamente\n");
+    printf("il file non e' stato aperto correttamente\n");
     return -1;
 } else
 
@@ -33,23 +53,23 @@ printf("file aperto correttamente\n");
 
 while (fgets(riga,MAX,fp))
 {
-        playlist[cont].numero = atoi(strtok(riga,","));
-        playlist[cont].titolo = strdup (strtok(NULL,","));
-        playlist[cont].artista = strdup(strtok(NULL,","));
+        (playlist+cont)->numero = atoi(strtok(riga,","));
+        (playlist+cont)->titolo = strdup (strtok(NULL,","));
+        (playlist+cont)->artista = strdup(strtok(NULL,","));
         cont++;
 }
     casuale(playlist, cont);
     fclose(fp);
     return 0;
 }
-void casuale(lista* playlist, int cont)
+void casuale(Lista* playlist, int cont)
 {
    int ripr, random;
    printf("numero di riproduzioni casuali: ");
    scanf("%d", &ripr);
    for (int a = 0; a < cont; a++)
               {
-                  playlist[a].ordine = false;
+                  (playlist+a)->ordine = false;
               }
           for (int n = 0; n < ripr; n++)                               //riproduzioni delle canzoni n volte
           {
@@ -59,11 +79,11 @@ void casuale(lista* playlist, int cont)
                   do
                   {
                      random = rand() % cont;
-                  } while (playlist[random].ordine);
-                  playlist[random].ordine = true;
-                  printf("sto riproducendo: %s di %s \n\n", playlist[random].titolo, playlist[random].artista);
+                  } while ((playlist+random)->ordine);
+                  (playlist+random)->ordine = true;
+                  printf("sto riproducendo: %s di %s \n\n", (playlist+random)->titolo, (playlist+random)->artista);    //print per sapere cosa sta riproducendo
               }
               for (int l = 0; l < cont; l++)
               {
-                  playlist[l].ordine = false;
+                  (playlist+l)->ordine = false;
               }}}
